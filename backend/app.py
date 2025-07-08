6b785f8e2758e7f1f618f239b9859c4899e3de02
theBullfish <140658834+theBullfish@users.noreply.github.com>
2025-07-08 02:41:08 -0500

    Delete backend directory

--- backend/app.py
+++
@@ -1,6 +0,0 @@
-from fastapi import FastAPI
-from .routes import biometrics, eeg
-
-app = FastAPI()
-app.include_router(biometrics.router, prefix="/api")
-app.include_router(eeg.router, prefix="/api")
